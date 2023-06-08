f15ff84ba0fd0b4c8bf0adcd56a083fab4cf1a71
Ivan Yavorksy <02ivan0971@gmail.com>
2023-06-08 19:20:06 +0300

    Update PaymentDue component

--- src/components/PaymentDue.tsx
+++ src/components/PaymentDue.tsx
@@ -4,15 +4,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 
 const PaymentDue: React.FC = () => {
+  const currentDate = new Date();
+  const currentMonth = currentDate.toLocaleString("default", { month: "long" });
+
   return (
     <div className="bg-white p-5 rounded-lg flex flex-col">
       <div className="text-sm text-left">No Payment Due</div>
       <div className="text-xs text-gray-500 text-left">
-        You have paid your September balance.
+        You have paid your {currentMonth} balance.
       </div>
       <div className="flex-grow"></div>
       <div className="flex justify-end items-end">
-        <div className="w-12 h-12">
+        <div>
           <FontAwesomeIcon icon={faCircleCheck} className="text-lg" />
         </div>
       </div>
