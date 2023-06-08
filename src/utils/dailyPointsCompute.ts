e2e729e592d7143222379984ca0039749338e35b
Ivan Yavorksy <02ivan0971@gmail.com>
2023-06-08 20:49:03 +0300

    Create utils

--- src/utils/dailyPointsCompute.ts
+++ src/utils/dailyPointsCompute.ts
@@ -1,28 +1,30 @@
-export default function calculatePoints(day, month) {
-  const seasons = {
+export default function calculatePoints(day: number, month: number): string {
+  const seasons: Record<string, number[]> = {
     winter: [12, 1, 2],
     spring: [3, 4, 5],
     summer: [6, 7, 8],
     autumn: [9, 10, 11],
   };
 
-  const seasonStartDates = {
-    winter: new Date().getFullYear() + "-12-01",
-    spring: new Date().getFullYear() + "-03-01",
-    summer: new Date().getFullYear() + "-06-01",
-    autumn: new Date().getFullYear() + "-09-01",
+  const seasonStartDates: Record<string, string> = {
+    winter: `${new Date().getFullYear()}-12-01`,
+    spring: `${new Date().getFullYear()}-03-01`,
+    summer: `${new Date().getFullYear()}-06-01`,
+    autumn: `${new Date().getFullYear()}-09-01`,
   };
 
   const currentSeason = Object.keys(seasons).find((season) =>
     seasons[season].includes(month)
   );
 
+  if (!currentSeason) {
+    throw new Error("Invalid season");
+  }
+
   const seasonStartDate = new Date(seasonStartDates[currentSeason]);
   const currentDate = new Date(seasonStartDate.getFullYear(), month - 1, day);
 
-  console.log(seasonStartDate);
-  console.log(currentDate);
-  const timeDiff = Math.abs(currentDate - seasonStartDate);
+  const timeDiff = Math.abs(currentDate.getTime() - seasonStartDate.getTime());
   const dayDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
 
   let points = 0;
