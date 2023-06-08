fecf4e080b40633f5150dff438a782d41c152885
Ivan Yavorksy <02ivan0971@gmail.com>
2023-06-08 19:19:11 +0300

    Update Balance component

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 
-const Balance: React.FC = () => {
+type BalanceProps = {
+  maxLimit: number;
+  balance: number;
+};
+
+const Balance: React.FC<BalanceProps> = ({ maxLimit, balance }) => {
   return (
     <div className="bg-white p-5 rounded-lg">
       <div className="text-sm text-left">Card Balance</div>
-      <div className="text-xl font-bold text-left">$17.30</div>
-      <div className="text-xs text-gray-500 text-left">$1343 Available</div>
+      <div className="text-xl font-bold text-left">${balance.toFixed(2)}</div>
+      <div className="text-xs text-gray-500 text-left">
+        ${(maxLimit - balance).toFixed(2)} Available
+      </div>
     </div>
   );
 };
